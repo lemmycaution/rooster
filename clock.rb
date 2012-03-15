c96454d0140946fbf1a5801dc538f36baca6bf55
Onur Uyar <fluxproject@gmail.com>
2012-03-15 12:26:30 +0200

    bugs are fixed

--- clock.rb
+++ clock.rb
@@ -2,42 +2,60 @@ require 'rubygems'
 require 'clockwork'
 require 'json'
 require 'twitter'
+
 include Clockwork
 include Twitter
 include JSON
 
+# load Twitter Credentials
+cred = YAML::load( File.open( 'cred.yml' ) )
+
+# Configure Twitter Credentials
 Twitter.configure do |config|
-  config.consumer_key = "3Ltc5g9E4UEmN3lAJj4iQ"
-  config.consumer_secret = "E7sO3zsr3Tnkofv7IEDWgAmREwNPidkXZwbyjqVP4"
-  config.oauth_token = "404387470-mpNAB6TM91ggskMrWDek0fKK3KWFYBqrEdDCX9Ok"
-  config.oauth_token_secret = "Jn4hBDsDgI1cX5Tmx8nuhY4k60xPFbAXzZl2wLpis"
+  config.consumer_key = cred['consumer_key']
+  config.consumer_secret = cred['consumer_secret']
+  config.oauth_token = cred['oauth_token']
+  config.oauth_token_secret = cred['oauth_token_secret']
 end
 
 # Initialize your Twitter client
 client = Twitter::Client.new
 
 handler do |job|
-  
+  # target tag
   tag = "gp"
+  # target user
   user = "masterlistweet"
+  # target feed
   list = "feed-list"
-  
+  # include entities to checking hashtags
   opts = {:include_entities => true}
+  # add since after first fetch to avoid duplicate posting
   opts = opts.merge({:since_id => @since_id}) if @since_id
-  puts "opts:#{opts.inspect}"
+  # gel list tweets
   list = Twitter.list_timeline(user,list,opts)
+  # integer to store tweet order num
   i = 0
+  
+  # loop in tweets
   list.each do |t|
-    unless t.entities.hashtags.empty?
-      t.entities.hashtags.find_all{|ht| ht.text==tag}.each do |gt|
-        @since_id = t.id if i==0
-        i+=1
 
-        puts client.update("#{t.text} @#{t.user.screen_name}")
+    # dive if it has hashtags
+    unless t.attrs["entities"].empty? && t.attrs["entities"]["hashtags"].empty?
+      # collect tweets with target hastag
+      t.attrs["entities"]["hashtags"].keep_if{|ht| ht["text"]==tag}.each do |gt|
+        # save since id
+        @since_id = t.id if i==0
+        # increment counter! important
+        i+=1
+        # retweet it on target account
+        puts client.update("#{t.text}")
       end
     end
+    
   end
 
 end
 
-every(1.minute, 'job_name')
+# do it every minute
+every(25.seconds, 'job_name')
